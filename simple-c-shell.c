5d698145308e94fe4de6abb6c8a65cecee069206
Radhesh Goel <radheshgoel3@gmail.com>
2023-12-02 03:21:43 +0530

    Fixed commit

--- simple-c-shell.c
+++ simple-c-shell.c
@@ -1,7 +1,7 @@
 /*
   * simple-c-shell.c
   *
-  * Copyright (c) 2023 Radhesh Goel.
+  * Copyright (c) 2023 Radhesh Goel
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
